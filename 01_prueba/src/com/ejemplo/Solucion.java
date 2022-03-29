40108dc552d2790a6d7720c28a3b23fe6352f457
Carmelo <Carmelo@equipo>
2022-03-29 20:08:29 +0200

    prueba

---
+++ 01_prueba/src/com/ejemplo/Solucion.java
@@ -0,0 +1,13 @@
+package com.ejemplo;
+
+public class Solucion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = 2;
+		int d = 3; // d = 1
+		
+		System.out.println(d-=c);
+	}
+
+}
